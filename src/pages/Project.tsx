c11829be267e1212b9f5809a931371d57b63de0f
milap-magar <milapmagar12@gmail.com>
2024-07-19 13:47:36 +0545

    Project page added

---
+++ src/pages/Project.tsx
@@ -0,0 +1,22 @@
+import { Container, Box, styled } from "@mui/material";
+import { Main, Navbar } from "../components";
+
+const WrapperContainer = styled(Container)({
+  width: "100%",
+  height: "100vh",
+  background:
+    "radial-gradient(ellipse, hsl(0, 0%, 64%) 0%, hsl(0, 1%, 16%) 100%, hsl(0, 0%, 41%) 50%)",
+});
+
+const Project = () => {
+  return (
+    <WrapperContainer maxWidth="xl" disableGutters>
+      <Box>
+        <Navbar />
+        <Main />
+      </Box>
+    </WrapperContainer>
+  );
+};
+
+export default Project;
